103adbd816a5df2be492fb9612f0fb40b5291b9c
Pavel Soriano <pavel.soriano@data.gouv.fr>
2019-03-13 14:16:10 +0100

    Requirements added. Fixed sh file

--- create_dataset.sh
+++ create_dataset.sh
@@ -4,17 +4,17 @@ conda activate pseudo-de-pseudonymizer
 
 python src/xml2txt.py "data/capp_legifrance/xml/*.xml" data/capp_legifrance/dataset --num 18
 
-# Replace pseudonyms by LOC, PER, DATE tags in train, dev, test
+echo "Replace pseudonyms by LOC, PER, DATE tags in train, dev, test"
 
-python src/tagger_capp.py data/capp_legifrance/dataset/train.txt /data/capp_legifrance/dataset/train_tagged.txt
+python src/tagger_capp.py data/capp_legifrance/dataset/train.txt data/capp_legifrance/dataset/train_tagged.txt
 
 python src/tagger_capp.py data/capp_legifrance/dataset/dev.txt data/capp_legifrance/dataset/dev_tagged.txt
 
 python src/tagger_capp.py data/capp_legifrance/dataset/test.txt data/capp_legifrance/dataset/test_tagged.txt
 
 
-# Transform tagged files into CoNLL format files
+echo "Transform tagged files into CoNLL format files"
 
-python src/txt2conll.py /data/capp_legifrance/dataset/train_tagged.txt
-python src/txt2conll.py /data/capp_legifrance/dataset/dev_tagged.txt
-python src/txt2conll.py /data/capp_legifrance/dataset/test_tagged.txt
+python src/txt2conll.py data/capp_legifrance/dataset/train_tagged.txt
+python src/txt2conll.py data/capp_legifrance/dataset/dev_tagged.txt
+python src/txt2conll.py data/capp_legifrance/dataset/test_tagged.txt
